c12e7377be3c1dd7eba067af0f66d1f00751bf75
josephe44 <josephe442@gmail.com>
2023-04-25 21:54:56 +0100

    express server setup

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("server");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to Eclock App API",
+  });
+});
+
+app.listen(PORT, () => console.log(`Server started ${PORT}`));
