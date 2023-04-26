7050c21060c98730c7352f5afad8a9c8a800a4df
josephe44 <josephe442@gmail.com>
2023-04-26 08:14:35 +0100

    error and exception handling

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// get data from body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.json({
@@ -10,6 +15,10 @@ app.get("/", (req, res) => {
   });
 });
 
+// Routes
 app.use("/api/users", require("./routes/userRoutes"));
+
+// Error Handler
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started ${PORT}`));
